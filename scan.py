a438949a10bccd44728f3715a4eb47a4eb2387a9
Daniel Ostrow <daniel.ostrow@pixee.ai>
2023-09-30 23:12:05 -0400

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	Test.md

--- scan.py
+++ scan.py
@@ -1,5 +1,18 @@
 import subprocess
 
+def scan():
+    print("Scanning...")
 subprocess.call(["arp", "-a"])
 subprocess.call(["arp", "-a"])
 
+def menu():
+    print("1. Scan")
+    print("2. Exit")
+    choice = input("Enter choice: ")
+    if choice == "1":
+        scan()
+    elif choice == "2":
+        exit()
+    else:
+        print("Invalid choice")
+        menu()
